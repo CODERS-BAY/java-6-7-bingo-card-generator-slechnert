0d0c1547f81405fe1e804413f7d9c32484751434
Simon <s.lechner.biz@gmail.com>
2020-02-24 14:53:24 +0100

    WiP, infantile stage at best

---
+++ src/com/codersbay/Bingo.java
@@ -0,0 +1,50 @@
+package com.codersbay;
+
+import java.util.Random;
+
+public class Bingo {
+    public static void main(String[] args) {
+        int[][] bingo = new int[4][4];
+        Random random = new Random();
+
+        int zahl1;
+        int zahl2;
+        int zahl3;
+        int zahl4;
+        int zahl5;
+
+        int zahl6;
+        int zahl7;
+        int zahl8;
+        int zahl9;
+        int zahl10;
+
+        int zahl11;
+        int zahl12;
+//        int zahl13;
+        int zahl14;
+        int zahl15;
+
+        int zahl16;
+        int zahl17;
+        int zahl18;
+        int zahl19;
+        int zahl20;
+
+        int zahl21;
+        int zahl22;
+        int zahl23;
+        int zahl24;
+        int zahl25;
+
+
+        for (int i = 0; i < 5; i++) {
+
+        }
+
+
+        bingo[2][2] = 0;
+        System.out.printf("%2d %2d %2d %2d %2d", bingo[][]);
+
+    }
+}
